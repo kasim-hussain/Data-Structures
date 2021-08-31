967d80a15cc7605eac5a322cc4e1ab3f3e1fc8c2
Kasim Hussain <46113197+kasim-hussain@users.noreply.github.com>
2021-08-31 12:34:42 +0100

    Add files via upload

---
+++ LinkedList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LinkedList<T>
+{
+    Node<T> head;
+
+    public LinkedList()
+    {
+        head = new Node<T>(default);
+    }
+
+    // Insert a Node at the end of the list.
+    public void Insert(T data)
+    {
+        Node<T> node = head;
+        Node<T> n = new Node<T>(data);
+
+        while (node.next != null) node = node.next;
+
+        node.next = n;
+        n.next = null;
+    }
+
+    // Delete the first occurence of data found within the list.
+    public void Delete(T data)
+    {
+        Node<T> current = head;
+        Node<T> previous = new Node<T>(default);
+
+        if (Search(data))
+        {
+            while (current != null && !EqualityComparer<T>.Default.Equals(current.data, data))
+            {
+                previous = current;
+                current = current.next;
+            }
+
+            previous.next = current.next;
+        }
+    }
+
+    // Search for data within the list.
+    public bool Search(T data)
+    {
+        Node<T> node = head;
+        while (node != null)
+        {
+            if (EqualityComparer<T>.Default.Equals(node.data, data))
+            {
+                Console.WriteLine($"{data} exists!");
+                return true;
+            }
+
+            node = node.next;
+        }
+        Console.WriteLine($"{data} does not exist!");
+        return false;
+    }
+
+    // Display the LinkedList.
+    public void Display()
+    {
+        Node<T> node = head;
+        while(node != null)
+        {
+            Console.Write($"[{node.data}]");
+            node = node.next;
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Node<T>
+{
+    public T data;
+    public Node<T> next;
+
+    public Node(T data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
